74f7c01d15536065b84c2bcaf86f1ba6df9ead6c
Tien <aim840912@gmail.com>
2020-02-28 18:24:52 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module"
+    },
+    env: {
+      node: true,
+      "jest/globals": true
+    },
+    extends: [
+      "airbnb-base",
+      "plugin:jest/recommended",
+      "plugin:prettier/recommended"
+    ],
+    plugins: ["jest", "prettier"],
+    rules: {
+      "import/prefer-default-export": "off",
+      "class-methods-use-this": "warn",
+      "consistent-return": "warn",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "no-continue": "off",
+      "no-bitwise": "off",
+      "no-underscore-dangle": "off",
+      "no-param-reassign": ["error", { props: false }],
+      "no-restricted-syntax": [
+        "error",
+        "ForInStatement",
+        "LabeledStatement",
+        "WithStatement"
+      ]
+    }
+  };
